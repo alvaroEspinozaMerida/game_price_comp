862ae008cc58e10f915ea0de70c15800cdfd25df
alvaroEspinozaMerida <111097688+alvaroEspinozaMerida@users.noreply.github.com>
2024-04-23 09:27:11 -0700

    deleted old comments

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,14 +1,11 @@
 import styles from "./Home.module.scss"
 import {useEffect, useState} from "react";
-// import testData from "../../../src/assets/content/test_data.json";
 
 import gameTestData from "../../assets/testData3.json"
 
 import SearchBar from "../SearchBar/index.jsx";
 import GameCarousel from "../GameCarousel/index.jsx";
 import GameItem from "../GameItem/index.jsx";
-
-import { Link } from 'react-router-dom';
 
 
 
@@ -39,39 +36,6 @@ const Home = () => {
                     </div>
                 </div >
 
-                {/*<div className={styles.main_container__popular_gallery}>*/}
-
-
-                {/*    <p>IMAGES FOR SOME PRODUCST WILL BE SHOWN HERE </p>*/}
-
-                {/*</div>*/}
-
-
-
-
-                {/*<h1>Games</h1>*/}
-                {/*/!*{console.log(content)}*!/*/}
-
-                {/*{content.map((data, index) =>(*/}
-                {/*    <div key = {index}>*/}
-                {/*        <GameItem gameTitle={data.name} prices={data.price_history}/>*/}
-                {/*    </div>*/}
-                {/*))}*/}
-
-
-
-
-
-                {/*<div className={styles.main_container__data_display}>*/}
-
-                {/*    {content.map((data, index.jsx) =>(*/}
-                {/*        <div key = {index.jsx}>*/}
-                {/*            <h1>{data.game}</h1>*/}
-                {/*            <h3>{data.date}:</h3>*/}
-                {/*            <GameItem price_history = {data.prices}/>*/}
-                {/*        </div>*/}
-                {/*    ))}*/}
-                {/*</div>*/}
 
             </div>
         </>
